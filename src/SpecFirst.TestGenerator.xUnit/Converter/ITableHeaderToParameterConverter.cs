06b6eeb60b5c22655b5c5e0ccb4557acc2939be4
Xiaoqing Zhang <xqzh76@gmail.com>
2021-05-20 16:58:50 +0100

    Split projects into separate repos

---
+++ src/SpecFirst.TestGenerator.xUnit/Converter/ITableHeaderToParameterConverter.cs
@@ -0,0 +1,10 @@
+﻿namespace SpecFirst.TestGenerator.xUnit.Converter
+{
+    using SpecFirst.Core.DecisionTable;
+    using SpecFirst.TestGenerator.xUnit.TestGeneration;
+
+    public interface ITableHeaderToParameterConverter
+    {
+        Parameter Convert(TableHeader tableHeader);
+    }
+}
