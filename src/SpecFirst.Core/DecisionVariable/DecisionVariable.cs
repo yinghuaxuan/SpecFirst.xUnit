4a2e7f66b3ae52a89bb73ce28f358b3590fb963c
Xiaoqing Zhang <xqzh76@gmail.com>
2021-06-05 11:02:39 +0100

    Add Parser and Validator for decision variable

---
+++ src/SpecFirst.Core/DecisionVariable/DecisionVariable.cs
@@ -0,0 +1,33 @@
+﻿namespace SpecFirst.Core.DecisionVariable
+{
+    using SpecFirst.Core.DecisionTable;
+    using System;
+    using System.Collections.Generic;
+
+    public class DecisionVariable
+    {
+        public DecisionVariable(string name, Type? type, object? value)
+        {
+            Name = name;
+            Type = type;
+            Value = value;
+            AssociatedTableHeader = new HashSet<TableHeader>();
+        }
+
+        public string Name { get; }
+        public Type? Type { get; set; }
+        public object? Value { get; set; }
+        public HashSet<TableHeader> AssociatedTableHeader { get; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is DecisionVariable variable &&
+                   Name == variable.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return 539060726 + EqualityComparer<string>.Default.GetHashCode(Name);
+        }
+    }
+}
