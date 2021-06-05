c49f3fcbae6b37339b4d2936445264605aa30517
Xiaoqing Zhang <xqzh76@gmail.com>
2021-06-05 11:02:39 +0100

    Add Parser and Validator for decision variable

---
+++ DecisionVariable/Parser/DecisionVariableParser.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace SpecFirst.Core.DecisionVariable.Parser
+{
+    public class DecisionVariableParser
+    {
+        public DecisionVariable Parse(XElement link)
+        {
+            string variableName = link.Attribute("title").Value.TrimStart('$');
+            string variableValue = link.Value;
+            return new DecisionVariable(variableName, typeof(string), variableValue);
+        }
+    }
+}
