a312048b3963df45e171e26381f0a1c6313381d6
Xiaoqing Zhang <xqzh76@gmail.com>
2021-06-06 14:01:48 +0100

    Parse variables in decision tables

--- DecisionTable/Parser/DecisionTableParser.cs
+++ DecisionTable/Parser/DecisionTableParser.cs
@@ -1,8 +1,10 @@
 ﻿namespace SpecFirst.Core.DecisionTable.Parser
 {
     using SpecFirst.Core.TypeResolver;
+    using SpecFirst.Core.DecisionVariable;
     using System;
     using System.Collections.Concurrent;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Xml.Linq;
 
@@ -19,13 +21,13 @@
             _tableDataParser = new TableDataParser();
         }
 
-        public DecisionTable Parse(XElement table)
+        public DecisionTable Parse(XElement table, IEnumerable<DecisionVariable> decisionVariables)
         {
             var tableName = _tableNameParser.Parse(table);
             var tableHeaders = _tableHeadersParser.Parse(table).ToArray();
             object[,] tableData = _tableDataParser.Parse(table, out Type[,] dataTypes);
             UpdateColumnTypesFromData(tableHeaders, dataTypes);
-            var variables = ExtractDecisionVariables(tableData, tableHeaders);
+            var variables = ParseDecisionVariables(tableData, tableHeaders, decisionVariables);
             return new DecisionTable(tableName, tableHeaders, tableData, variables);
         }
 
@@ -61,24 +63,83 @@
             return array;
         }
 
-        private static DecisionVariable.DecisionVariable[] ExtractDecisionVariables(object[,] tableData, TableHeader[] tableHeaders)
+        private static DecisionVariable[] ParseDecisionVariables(
+            object[,] tableData,
+            TableHeader[] tableHeaders,
+            IEnumerable<DecisionVariable> definedVariables)
         {
-            var variables = new ConcurrentDictionary<string, DecisionVariable.DecisionVariable>();
+            var variables = ExtractDecisionVariables(tableData, tableHeaders);
+            foreach (var variable in variables)
+            {
+                var header = variable.AssociatedTableHeaders.ElementAt(0);
+                if (variable.AssociatedTableHeaders.Any(h => h.TableHeaderType != header.TableHeaderType))
+                {
+                    throw new InvalidOperationException($"{variable.Name} variable is associated with both input and output headers");
+                }
+
+                switch (header.TableHeaderType)
+                {
+                    case TableHeaderType.Input:
+                    case TableHeaderType.Comment:
+                        UpdateInputVarialbe(definedVariables, variable);
+                        break;
+                    case TableHeaderType.Output:
+                        UpdateOutputVariable(ref definedVariables, variable);
+                        break;
+                }
+            }
+
+            return variables.ToArray();
+        }
+
+        private static IEnumerable<DecisionVariable> UpdateOutputVariable(ref IEnumerable<DecisionVariable> definedVariables, DecisionVariable variable)
+        {
+            if (definedVariables.Contains(variable))
+            {
+                throw new InvalidOperationException($"{variable.Name} variable is assigned as an output but it is already defined");
+            }
+            else
+            {
+                variable.Type = typeof(object);
+                definedVariables = definedVariables.Union(new[] { variable });
+            }
+
+            return definedVariables;
+        }
+
+        private static void UpdateInputVarialbe(IEnumerable<DecisionVariable> definedVariables, DecisionVariable variable)
+        {
+            if (!definedVariables.Contains(variable))
+            {
+                throw new InvalidOperationException($"{variable.Name} variable is assigned as an input but it is not defined");
+            }
+            else
+            {
+                var temp = definedVariables.First(v => v == variable);
+                variable.Type = temp.Type;
+                variable.Value = temp.Value;
+            }
+        }
+
+        private static IEnumerable<DecisionVariable> ExtractDecisionVariables(object[,] tableData, TableHeader[] tableHeaders)
+        {
+            var variables = new ConcurrentDictionary<string, DecisionVariable>();
+
             for (int i = 0; i < tableData.GetLength(0); i++)
             {
                 for (int j = 0; j < tableData.GetLength(1); j++)
                 {
-                    if (tableData[i, j] is DecisionVariable.DecisionVariable variable)
+                    if (tableData[i, j] is DecisionVariable variable)
                     {
                         variables.AddOrUpdate(
                             variable.Name,
                             variable,
-                            (key, value) => { variable.AssociatedTableHeader.Add(tableHeaders[j]); return variable; });
+                            (key, value) => { variable.AssociatedTableHeaders.Add(tableHeaders[j]); return variable; });
                     }
                 }
             }
 
-            return variables.Values.ToArray();
+            return variables.Values;
         }
     }
 } 
